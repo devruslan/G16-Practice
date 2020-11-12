2aba75677aaefe817edcab8fb8e8304ae68b3a93
trudova <liubovtrudova@gmail.com>
2020-11-12 12:54:42 -0500

    Liv - additional

---
+++ src/LiubovTrudova/Additional.java
@@ -0,0 +1,10 @@
+package LiubovTrudova;
+
+public class Additional {
+    public static void main(String[] args) {
+
+    }
+    public static void Additional(){
+        System.out.println("Added additional functionality");
+    }
+}
